68a2acfd384723f07e1acfa42c0d50186e8f6e58
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-08-04 04:12:49 +0600

    product post done

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-// app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.json());
+// app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
@@ -28,5 +28,27 @@ app.get('/product', (req, res) => {
             res.send(result.rows);
         }
     });
+    client.end;
+})
+app.get('/products', (req, res) => {
+    client.query(`Select * from products`, (err, result) => {
+        if (!err) {
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+app.post('/products', (req, res) => {
+    const user = req.body;
+    console.log(user.p_title)
+    let insertQuery = `insert into products(p_title, p_categories, p_description, p_price,p_rent,p_rentoption,p_user) 
+                       values('${user.p_title}', '${user.p_categories}', '${user.p_description}', '${user.p_price}','${user.p_rent}','${user.p_rentoption}','${user.p_user}')`
+
+    client.query(insertQuery, (err, result) => {
+        if (!err) {
+            res.send(result);
+        }
+        else { console.log(err.message) }
+    })
     client.end;
 })
